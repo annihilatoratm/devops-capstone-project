d002a0d34a0d9941ff5bec7bc079f701156cf369
Fuad Huseynov <106423700+annihilatoratm@users.noreply.github.com>
2023-11-24 13:46:42 +0400

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -63,6 +63,7 @@ def create_accounts():
 
 # ... place you code here to LIST accounts ...
 
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -83,6 +84,7 @@ def list_accounts():
 
 # ... place you code here to READ an account ...
 
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
@@ -101,6 +103,7 @@ def get_accounts(account_id):
 ######################################################################
 
 # ... place you code here to UPDATE an account ...
+
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
